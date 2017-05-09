ea2291578410df1970f3d50efaf32f9c18545254
cyx <chenyixiong@wusongtech.com>
2017-05-09 17:56:44 +0800

    test commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        int i=0;
+
+        i=10;
+
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
+        i=20;
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,21 @@
+/**
+ * Created by 66170 on 2017/5/9.
+ */
+public class User {
+    private int id;
+    private String name;
+    private Integer age;
+
+    public User() {
+    }
+
+    public void run() {
+        try {
+            name = "cyx";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
